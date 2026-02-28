f5dd228e9006ddaa0c3c6a0bdaeca9ccb75a18d0
redahablouji-cmd <redahablouji@gmail.com>
2026-02-28 17:21:42 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'FST SERVE',
   description: 'Fast EV Charging Service',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
 };
 
 export default function RootLayout({
