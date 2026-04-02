f8c89899635c5b086022fe152ea3b734b2b488cf
redahablouji-cmd <redahablouji@gmail.com>
2026-04-02 14:44:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2175,8 +2175,8 @@ if (!customerName || !customerPhone) {
                       <p className="text-gray-500 font-medium">No past charges found.</p>
                     </div>
                   ) : (
-                    {activeOrders.filter(order => order.status && !order.status.includes('Canceled')).map((order, index) => (
-                      <div key={idx} className="bg-white p-5 rounded-[24px] flex flex-col gap-3 shadow-sm border border-gray-100">
+                    activeOrders.filter(order => order.status && !order.status.includes('Canceled')).map((order, index) => (
+  <div key={index} className="bg-white p-5 rounded-[24px] flex flex-col gap-3 shadow-sm border border-gray-100">
                          <div className="flex justify-between items-start">
                             <span className="font-bold text-[#1C1C1E] text-lg">{order.vehicle}</span>
                             <span className="text-xs font-bold px-3 py-1.5 bg-gray-100 text-[#1C1C1E] rounded-xl">
